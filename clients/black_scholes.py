f52922ee324baf45a0f137ff87d4b3f176aa26d9
Reagan <reamil@umich.edu>
2021-03-20 12:28:09 -0400

    implemented brownian motion class, copied example algo

---
+++ clients/black_scholes.py
@@ -0,0 +1,13 @@
+from scipy.stats import norm
+import math
+
+
+class BlackScholes:
+    def calc_call(self):
+        pass
+
+    def calc_put(self):
+        pass
+
+    def __init__(self):
+        pass
---
+++ clients/brownian.py
@@ -0,0 +1,32 @@
+import numpy as np
+import math
+
+
+"""
+Implements a basic Geometric Brownian Motion simulation to simulate 
+the random continuous motion of a specified equity. 
+
+Recommended parameters:
+DRIFT -- Average return over the time period T
+VOLATILITY -- Implied volatility or other simplistic approximation
+"""
+
+
+class Brownian:
+    def simulate_paths(self):
+        while self.T - self.dt > 0:  # While time remains
+            dWt = np.random.normal(0, math.sqrt(self.dt))
+            dYt = self.drift * self.dt + self.volatiltiy * dWt  # Change in price
+            self.current_price += dYt  # Apply price change
+            self.prices.append(self.current_price)
+
+            self.T -= self.dt  # Account for time interval
+
+    def __init__(self, price, drift, volatility, dt, T):
+        self.current_price = self.initial_price = price
+        self.drift = drift
+        self.volatiltiy = volatility
+        self.dt = dt
+        self.T = T
+        self.prices = []
+        self.simulate_paths()
