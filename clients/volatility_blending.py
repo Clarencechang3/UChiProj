9de4b3c37a8166383ad99b8102ae36aa71d6e884
Reagan <reamil@umich.edu>
2021-04-08 18:29:38 -0400

    corrected arch usage

--- clients/volatility_blending.py
+++ clients/volatility_blending.py
@@ -2,6 +2,7 @@ import volatility
 import numpy as np
 import arch
 
+
 def blend(returns) -> float:
     """
     Blends the volatility estimates of the selected volatility models.
@@ -9,23 +10,23 @@ def blend(returns) -> float:
     """
     # if it's the first day, don't want to volatility blend as this will be highly inaccurate
     # Instead, use a pre-computed volatility (average volatility of sample paths)
-    if (len(returns) < 200):
+    if len(returns) < 200:
         return 1.6879372939912174
 
     alpha_1 = 0.4
-    garch = arch_model(returns, vol='garch', p=1, o=0, q=1)
+    garch = arch.arch_model(returns, vol="garch", p=1, o=0, q=1)
     garch_fitted = garch.fit()
     g_pred = garch_fitted.forecast()
 
     alpha_2 = 0.3
-    figarch = arch_model(returns, vol='figarch', p=1)
+    figarch = arch.arch_model(returns, vol="figarch", p=1)
     figarch_fitted = figarch.fit()
     f_pred = figarch_fitted.forecast()
 
     alpha_3 = 0.3
-    egarch = arch_model(returns, vol='egarch', p=1, o=1, q=1)
+    egarch = arch.arch_model(returns, vol="egarch", p=1, o=1, q=1)
     egarch_fitted = egarch.fit()
     e_pred = egarch_fitted.forecast()
 
     # convert variance to volatility and return value
-    return alpha_1*g_pred + alpha_2*f_pred + alpha_3*e_pred
+    return alpha_1 * g_pred + alpha_2 * f_pred + alpha_3 * e_pred
