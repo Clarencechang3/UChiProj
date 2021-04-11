2e39dbe5cc61525c551fd5483849eafa844aca19
Andrew Ferruolo <andrew.ferruolo@gmail.com>
2021-04-10 22:12:48 -0400

    Finished first optimization

--- portfolio/allocate.py
+++ portfolio/allocate.py
@@ -1,7 +1,9 @@
+
 import numpy as np
 from numpy.linalg import inv
 import pandas as pd
 from scipy.optimize import newton
+
 
 
 # List of valid assets in portfolio
@@ -36,11 +38,9 @@ assets = [
 n_assets = len(assets)
 
 # Global storage of price data
-historical_returns = pd.read_csv("Case3HistoricalPrices.csv", index_col=0)
+historical_returns = pd.read_csv("portfolio/Case3HistoricalPrices.csv", index_col=0)
 
 
-
-#########################Calculate Risk Aversion ##############################################
 # Uses 180-day volatility weighting
 risk = np.asarray(historical_returns.iloc[-180:].std())
 weighted = np.divide(np.repeat(1, n_assets), risk)
@@ -50,52 +50,48 @@ total = np.sum(weighted)
 market_weights = weighted / total
 
 #covariance matrix of returns, since risk free rate is 0, don't need to calculate excess returns
-cov = historical_returns.cov()
+cov = historical_returns.cov().to_numpy()
 
 #return and variance of the market portfolio
-global_return = historical_returns.mean().multiply(market_weights['weights'].values).sum()
-global_variance = np.matmul(market_weights.values.T, np.matmul(cov.values, market_weights.values))
+global_return = historical_returns.mean().multiply(market_weights).sum()
+market_var = np.matmul(market_weights.reshape(len(market_weights)).T,
+                                       np.matmul(cov, market_weights.reshape(len(market_weights))))
+risk_aversion = global_return / market_var
 
-risk_aversion = global_return / global_variance    
-######################### Calculate Implied Returns##############################################
-#implied equilibrium returns of all assets
+calculate_implied_returns = lambda weights : risk_aversion * cov.dot(weights)
+# reg_implied_returns = calculate_implied_returns(market_weights)
 
-implied_returns_eq = lambda weights : risk_aversion * cov.dot(weights)
-optim_weights = newton(implied_returns_eq, market_weights) 
+implied_returns_train = lambda weights : (risk_aversion * cov.dot(weights))**2
+optim_weights = newton(implied_returns_train, market_weights) 
+implied_returns = calculate_implied_returns(optim_weights)
 
-implied_returns = implied_returns_eq(optim_weights) 
-
-##########################CALCULATE VIEWS ######################################################
-#Constants
 n_views = 3
 
-Q = np.array([0.0925, 0.005, 0.055])
-P = np.ones((n_views, n_assets))
-Omega = 
+P = np.array([
+    market_weights.tolist(),#Market Cap
+    (1 - market_weights).tolist(),#1 - Market Cap
+    historical_returns.var().to_list()
+])
 
-
-
-
-
-
-
-
-
-
-#Array of views
-Q = np.array() 
-#P represents the matrix identifying assets involved in each view
-P = np.asarray([[]])
-
-#covariance matrix of errors of our views
-#should be a diagonal matrix
-def error_cov_matrix(cov_matrix, tau, P):
-    matrix = np.diag(np.diag(tau(cov).dot(P.T)))
+def error_cov_matrix(sigma, tau, P):
+    matrix = np.diag(np.diag(P.dot(tau * cov).dot(P.T)))
     return matrix
-tau = 1
+tau = 0.015
 omega = error_cov_matrix(cov, tau, P)
 scaled_cov = cov * tau
-BL_returns = implied_returns + scaled_cov.dot(P.T).dot(inv(P.dot(scaled_cov).dot(P.T) + omega).dot(Q - P.dot(implied_returns)))
+
+train_returns = lambda Q_arg : implied_returns + \
+            scaled_cov.dot(P.T).dot(inv(P.dot(scaled_cov).dot(P.T) + omega).dot(Q_arg - P.dot(implied_returns)))
+
+
+def deriv(Q_arg):
+    h = 0.001
+    return (train_returns(Q_arg + h) - train_returns(Q_arg - h))/(2*h)
+
+Q = np.array([1.5, -0.5, -1])
+# Q_optim = newton(train_returns, Q, deriv)
+
+BL_returns = train_returns(Q).sum()
 
 BL_weights = inv(cov.values).dot(BL_returns)
 BL_weights = BL_weights/sum(BL_weights)
@@ -116,24 +112,60 @@ BL_weights = BL_weights/sum(BL_weights)
 def allocate_portfolio(asset_prices):
     # Add any new pricing information passed into function
     historical_returns.append(asset_prices)
+    # Uses 180-day volatility weighting
+    risk = np.asarray(historical_returns.iloc[-180:].std())
+    weighted = np.divide(np.repeat(1, n_assets), risk)
+    total = np.sum(weighted)
 
-    weights = np.repeat(1 / n_assets, n_assets)
-    return weights
+    # Ensure weights add to 1
+    market_weights = weighted / total
+
+    #covariance matrix of returns, since risk free rate is 0, don't need to calculate excess returns
+    cov = historical_returns.cov().to_numpy()
+
+    #return and variance of the market portfolio
+    global_return = historical_returns.mean().multiply(market_weights).sum()
+    market_var = np.matmul(market_weights.reshape(len(market_weights)).T,
+                                        np.matmul(cov, market_weights.reshape(len(market_weights))))
+    risk_aversion = global_return / market_var
+
+    calculate_implied_returns = lambda weights : risk_aversion * cov.dot(weights)
+    # reg_implied_returns = calculate_implied_returns(market_weights)
+
+    implied_returns_train = lambda weights : (risk_aversion * cov.dot(weights))**2
+    optim_weights = newton(implied_returns_train, market_weights) 
+    implied_returns = calculate_implied_returns(optim_weights)
+
+    n_views = 3
+
+    P = np.array([
+        market_weights.tolist(),#Market Cap
+        (1 - market_weights).tolist(),#1 - Market Cap
+        historical_returns.var().to_list()
+    ])
+
+    def error_cov_matrix(sigma, tau, P):
+        matrix = np.diag(np.diag(P.dot(tau * cov).dot(P.T)))
+        return matrix
+    tau = 0.015
+    omega = error_cov_matrix(cov, tau, P)
+    scaled_cov = cov * tau
+
+    train_returns = lambda Q_arg : implied_returns + \
+                scaled_cov.dot(P.T).dot(inv(P.dot(scaled_cov).dot(P.T) + omega).dot(Q_arg - P.dot(implied_returns)))
 
 
+    def deriv(Q_arg):
+        h = 0.001
+        return (train_returns(Q_arg + h) - train_returns(Q_arg - h))/(2*h)
 
+    Q = np.array([1.5, -0.5, -1])
+    # Q_optim = newton(train_returns, Q, deriv)
 
+    BL_returns = train_returns(Q).sum()
 
+    BL_weights = inv(cov.values).dot(BL_returns)
+    BL_weights = BL_weights/sum(BL_weights)
+    return BL_weights
 
-##Old Code
-
-#implied equilibrium returns of all assets
-def implied_rets(risk_aversion, cov_matrix, weights):
-    returns = risk_aversion * cov_matrix.dot(weights)
-    return returns
-
-
-
-
-implied_returns = implied_rets(risk_aversion, cov, market_weights)
 
